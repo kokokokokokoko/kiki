207d2bcc6bd8a8f06866fe83b3782df46514d610
James <pinecone@pak0.com>
2018-09-08 15:02:14 -0700

    Update kiki.py

--- kiki.py
+++ kiki.py
@@ -53,11 +53,11 @@ class Td:
 
 
 
-token=''
-apikey ='xxxxxx@AMER.OAUTHAP'
+refresh_token=''
+access_token =''
 client_id = 'xxxxxxx@AMER.OAUTHAP'
 
-p = Td(token,client_id,apikey)
+p = Td(refresh_token,client_id,access_token)
 start_date = datetime.strptime('04 3 2018  1:33PM', '%m %d %Y %I:%M%p')
 end_date = datetime.strptime('05 3 2018  1:33PM', '%m %d %Y %I:%M%p')
 print(p.get_price_history('SNAP',p.unix_time_millis(start_date),p.unix_time_millis(end_date)))
